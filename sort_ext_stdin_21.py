1d55cd5b0850069daded5b499fdaafdb2c93c387
dshk <notabenaabakan@gmail.com>
2022-08-18 11:58:56 +0300

    Daily report. Exercise.

--- sort_ext_stdin_21.py
+++ sort_ext_stdin_21.py
@@ -4,6 +4,7 @@ import argparse, sys
 parser = argparse.ArgumentParser()
 parser.add_argument('file', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help="input file to be sorted")
 parser.add_argument('-u', '--unique', action = 'store_true', help = "sort uniquely")
+parser.add_argument('-o', '--output', type=argparse.FileType('w'))
 args = parser.parse_args()
 
 ip_lines = args.file.readlines()
@@ -11,5 +12,9 @@ if args.unique:
     ip_lines = set(ip_lines)
 
 op_lines = sorted(ip_lines, key=lambda s: (s.rsplit('.', 1)[-1], s))
-for line in op_lines:
-    print(line, end='')
+if args.output:
+    for line in op_lines:
+        args.output.write(line)
+else:
+    for line in op_lines:
+        print(line, end='')
